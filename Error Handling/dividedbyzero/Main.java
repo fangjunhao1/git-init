c61d4a6641a1e9b4ec33e28743b4c589039d881b
xiaorrrr <1092676441@qq.com>
2024-03-20 20:27:31 +0800

    Exception specified

--- Error Handling/dividedbyzero/Main.java
+++ Error Handling/dividedbyzero/Main.java
@@ -1,18 +1,15 @@
 package dividedbyzero;
 
-public class Main {
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         try {
-            int result = divideByZero();
-            System.out.println("Result: " + result);
-        } catch (Exception e) {
+            System.out.println(3/0);
+        } catch (ArithmeticException e) {
             System.out.printf("Caught runtime exception = %s\n", e.getMessage());
         }
-    }
-
-    public static int divideByZero() {
-        int numerator = 10;
-        int denominator = 0;
-        return numerator / denominator;
     }
 }
